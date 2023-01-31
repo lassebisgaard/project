2c32be2e311b5dff2b20db0182e156ce6324dcd4
lassebisgaard <Lasse_4321@hotmail.com>
2023-01-31 10:15:12 +0100

    java prøve

---
+++ style.js
@@ -0,0 +1,4 @@
+// bruges til at tjekke om ens javascript kører som det skal
+console.log("Lasse");
+// går ind i det indre HTML og tilføjer en header
+document.body.innerHTML += "<h1> hej med dig </h1>";
